b51d9ecdb4d9462874bb2bdaf8080446cb261d05
+dennis.mutua@student.moringaschool.com <dennis@DESKTOP-Q9A0DBN.localdomain>
2023-12-16 17:32:52 +0300

    lab completed

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,4 @@
-describe('employees', function() {
+git describe('employees', function() {
   describe('updateEmployeeWithKeyAndValue(employee, key, value)', function () {
     beforeEach(function () {
       for (const key in employee) {
